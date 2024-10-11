588936a7c4513f2ba2ec39a3b9bdf018a3dcf599
tartamudeando1 <135078057+tartamudeando1@users.noreply.github.com>
2024-10-11 11:44:39 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,39 @@
+criaCartão('quimica','Assinale a alternativa que indica apenas materiais constituídos de Carbono. ( responda com base no video explicativo)','Fibra de carbono, combustíveis, carvão, grafite e diamante' )
+
+criaCartão('quimica','Como é a distribuição eletrônica do Carbono, seguindo os conceitos de Linus Pauling=','1s2; 2s2; 2p2' )
+
+criaCartão('quimica','Qual o número atômico do Carbono?','6' )
+
+criaCartão('quimica','Em qual período da tabela periódica o Carbono está?','Período 7' )
+
+criaCartão('quimica','Em qual família o Carbono se encontra?','Família 14A' )
+
+criaCartão('geografia','Na proporção dos gases existentes no ar da troposfera o Gás Oxigênio ocupa-talvez mais ou talvez menos- 19%, enquanto outros gases(exceto o Gás Nitrogênio) ocupam 1%. E o Gás Nitrogênio ?','78%' )
+
+criaCartão('geografia','É verdade que o Gás Carbônico não serve só para os seres clorofilados realizarem a fotossíntese?','sim' )
+
+criaCartão('geografia','Um ser aeróbico é um ser:','Seres que utilizam Gás Oxigênio em sua respiração.' )
+
+criaCartão('geografia','seres clorofilados (que realizam a fotossíntese)transformam o Gás carbônico em Gás oxigênio. Um aluno de uma classe,ouvindo a explicação da professora, afirmou que seres clorofilados podem também respirar e que nessa respiração as plantas absorvem o Gás oxigênio e o transforma em Gás carbônico. Você concorda com esta afirmação?','sim' )
+
+criaCartão('geografia','Quais os gases nobres?','Gás randônio, gás hélio, gás xenônio, gás neônio, gás argônio e gás criptônico' )
+
+criaCartão('biologia','em um banco de sangue contém 10 litros de tipo A, 5 de tipo B, 9 de tipo AB e 7 de tipo o. Um paciente de tipo sanguíneo A está a espera de uma transfusão. Quantos litros de sangue estão disponível no bando de sangue que seja compatível com o seu?','17' )
+
+criaCartão('biologia','Sabemos que, além do DNA cromossômico, a bactéria possui plasmídeos. Marque a alternativa que indica corretamente o que é um plasmídeo.','Pequenas moléculas de BNA circular' )
+
+criaCartão('biologia','Sabemos que as bactérias possuem uma grande variedade de formas e modos de vida, sendo que algumas vivem solitárias e outras formam colônias. Podemos classificar as bactérias de acordo com seu formato e agrupamento. Marque a alternativa que contém apenas formas de bactérias.','Cocos, espirolos e bacilos' )
+
+criaCartão('biologia','As bactérias são os menores seres vivos conhecidos.Essa afirmação é:','falsa')
+
+criaCartão('biologia','As bactérias são seres vivos que na maioria das vezes são associados a doenças e decomposição da matéria orgânica, como de alimentos. Entretanto, sabemos que elas também são úteis aos seres humanos. A respeito da estrutura desses seres, marque a alternativa que NÃO indica uma característica presente em bactérias.','Não possuem menbrana plasmática' )
+
+criaCartão('historia','O Período Regencial no Brasil durou nove anos. Durante esse tempo tiveram quatro períodos de diferentes regências, são elas em ordem de início e fim:','Regência trina provisória, Regência trina permanente, Regência Una de Feijó, Regência Una de Araújo lima' )
+
+criaCartão('historia','Durante o Período Regencial (1831-1840), houveram algumas revoltas, podem ser incluídas:','A Cabanagem, a Balaiada e a Revolta Farroupilha' )
+
+criaCartão('historia','Os chamados socialistas utópicos que surgiram após a Revolução Industrial tinham como objetivo:','O socialismo utópico foi uma corrente de pensamento socialista que surgiu na Europa na virada do século XVIII para XIX. Essa corrente socialista promovia críticas ao capitalismo e à miséria e desigualdade social que existia dentro desse sistema. Acreditava-se ser necessário combater esses problemas. Os socialistas utópicos acreditavam que reformas seriam suficientes para construir uma sociedade ideal, baseada na igualdade e cooperação. Eles não acreditavam na saída revolucionária; muitos não defendiam a superação do capitalismo e achavam que a própria burguesia poderia comandar o processo de reformas para combater os problemas mencionado')
+
+criaCartão('historia','Após as 13 colônias começarem a negociar com comerciantes, a Inglaterra aprovou uma sequência de leis, a primeira delas foi:','A lei do Açúcar: todo açúcar consumido na colônia deve passar pela autorização e pagamento de impostos para a Coroa' )
+
+criaCartão('historia','Durante o período das 13 colônias, houve uma guerra entre o Sul e o Norte. O Sul se caracterizava pela mão de obra escrava e a exportação para a Inglaterra. O Norte se caracterizava pelas industrias e o mercado baseado no protecionismo. A guerra citada é:','A guerra de sucessão' )
