17500436eca6664b23bde1265c640f420ef18e1e
tartamudeando1 <135078057+tartamudeando1@users.noreply.github.com>
2024-10-18 12:06:23 -0300

    .

--- main.js
+++ main.js
@@ -13,5 +13,15 @@ function criaCartão(categoria, pergunta, resposta){
     </div>
 </div>
     `
+
+    let respostaEstaVisivel = false;
+    function viraCartao(){
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+
+    cartao.addEventListener('click', viraCartao)
+
+
     container.appendChild(cartao);
 }
